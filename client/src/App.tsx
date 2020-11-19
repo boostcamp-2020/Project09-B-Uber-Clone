7de655924062dab28796901c7b82ebe6b4b97df1
2hoyeong <44524462+2hoyeong@users.noreply.github.com>
2020-11-19 16:05:27 +0900

    [common] chore (#1) 기본 환경설정
    eslint, prettier, typescript, express, issue template, pr template, gitignore 설정완료했습니다.

---
+++ client/src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return <div>hello world</div>;
+};
+
+export default App;
---
+++ server/app.ts
@@ -0,0 +1,30 @@
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+import indexRouter from './routes/index';
+
+const app: express.Application = express();
+
+// view engine setup
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ message: err.message });
+});
+
+module.exports = app;
