69bd42078b9b3f25a9e4f6751f9a3996e92b7411
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-23 23:02:25 +0900

    [fe] feat (#4) 페이지 추가 - 라우팅에 맞게 각 페이지 추가   - not found page 추가 - 페이지를 묶어서 export 할 index 파일 추가

---
+++ client/src/pages/driver/DriverAuthPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DriverAuthPage: React.FC = () => {
+  return <div>드라이버 로그인, 회원가입 선택 페이지</div>;
+};
+
+export default DriverAuthPage;
---
+++ client/src/pages/user/UserSigninPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserSigninPage: React.FC = () => {
+  return <div>유저 로그인 페이지</div>;
+};
+
+export default UserSigninPage;
