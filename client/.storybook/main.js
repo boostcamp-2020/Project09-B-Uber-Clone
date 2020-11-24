bd7ef0cd786f22f6398d6a133563a77f5ec6a4e4
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-24 15:23:41 +0900

    [fe] chore (#2) 스토리북에 antd-mobile 스타일 적용 - storybook main.js에 babel plugin 적용

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-knobs'],
+  babel: async (options) => ({
+    ...options,
+    plugins: [['import', { libraryName: 'antd-mobile', style: 'css' }]],
+  }),
 };
