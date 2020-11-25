6d2ccf812cf64399c5df662b3eb7e2cfc1da0809
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-25 13:44:48 +0900

    [be] feat (#19) ApolloServer context 분리 - 요청 시마다 실행될 context 함수 분리 - 토큰 정보가 필요없는 요청은 별도 list 생성   - 해당 요청일 경우 인증 과정을 거치지 않고 resolver 실행   - 이 때 res를 보내 쿠키를 설정할 수 있게 함 - 인증이 필요한 로직을 TODO에 추가

---
+++ server/graphql/context.ts
@@ -0,0 +1,24 @@
+// import Models from '../models';
+import authenticateNotRequired from '../utils/authenticateNotRequired';
+
+const context = ({ req, res }) => {
+  const { operationName } = req.body;
+
+  if (authenticateNotRequired.includes(operationName)) return { res };
+
+  /**
+   * // TODO: 로그인, 회원가입 외 일반 요청 시 존재하는 유저인지 확인
+   * // 있다면 해당 user return, 없다면 오류 발생
+   *
+   * const cookie = req.cookies.userToken;
+   *
+   * // cookie decode
+   *
+   * const userSchema = Models.model('User');
+   * const newUser = { 위에서 찾은 유저 정보 };
+   * if (newUser) return { user: 찾은 유저 };
+   * else 오류 전송
+   */
+};
+
+export default context;
---
+++ server/utils/authenticateNotRequired.ts
@@ -0,0 +1,3 @@
+const authenticateNotRequired = ['userSignin', 'userSignup', 'driverSignin', 'driverSignup'];
+
+export default authenticateNotRequired;
