287237dcc2506365d5ef67c0e109961b2f4bc818
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-25 13:47:59 +0900

    [be] feat (#19) 드라이버 회원가입 mutation 구현 - 입력받은 정보를 DB에 저장 - 실패 시 success를 false로 설정하고 message와 함께 return - 성공 시   - res에 토큰 설정   - sucess를 true로 설정해 return

---
+++ server/graphql/resolver/mutation.ts
@@ -0,0 +1,15 @@
+const Mutation = {
+  driverSignup: async (_, args, { dataSources, res }) => {
+    const driverSchema = dataSources.model('Driver');
+    const newDriver = new driverSchema(args);
+    const result = await newDriver.save();
+
+    if (!result) return { success: false, message: '회원가입 중 오류가 발생했습니다' };
+
+    // TODO: 토큰 생성
+    res.cookie('userToken', 'CREATED_TOKEN');
+    return { success: true };
+  },
+};
+
+export default Mutation;
