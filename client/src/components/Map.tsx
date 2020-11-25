32a6ca767d93a165d3a051b01325800777bb8386
2hoyeong <44524462+2hoyeong@users.noreply.github.com>
2020-11-25 15:21:01 +0900

    [fe] fix (#12) redux 적용
    맵 컴포넌트에 redux를 적용했습니다.

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,74 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import GoogleMapReact from 'google-map-react';
 import Marker from './Marker';
 import { Location } from '../types';
 
-const Map: React.FC<{ center: Location; zoom: number }> = ({ center, zoom }) => {
-  const [location, setLocation] = useState(center);
-  const [myLocation, setMyLocation] = useState(center);
 
-  const updateLocation = async () => {
-    try {
-      const myLocation: Location = await getLocation();
-      setMyLocation(myLocation);
-    } catch (error) {
-      alert('GPS가 사용이 불가능합니다.');
-    }
-  };
+const Map: React.FC<{
+  center: Location;
+  location: Location;
+  zoom: number;
+  updateMyLocation: () => void;
+  moveCenterMyLocation: () => void;
+}> = ({ center, location, zoom, updateMyLocation, moveCenterMyLocation }) => {
+  useEffect(() => {
+    setInterval(updateMyLocation, 1000);
+  }, []);
 
-  const moveCenterMyLocation = async () => {
-    try {
-      const location = await getLocation();
-      setLocation(location);
-    } catch (error) {
-      alert('GPS가 사용이 불가능합니다.');
-    }
-  };
-
-  setTimeout(updateLocation, 3000);
   return (
     <div style={{ height: '100vh', width: '100%' }}>
-      <div>
-        {location.lat}, {location.lng}
-      </div>
       <GoogleMapReact
         bootstrapURLKeys={{ key: API_KEY }}
         defaultZoom={zoom}
-        center={location}
+        center={center}
         onTilesLoaded={() => {
-          updateLocation();
+          updateMyLocation();
           moveCenterMyLocation();
         }}
       >
-        <Marker lat={myLocation.lat} lng={myLocation.lng} color="#95A5A6" />
+        <div>
+          {center.lat}, {center.lng}
+        </div>
+        <Marker lat={location.lat} lng={location.lng} color="#95A5A6" />
       </GoogleMapReact>
     </div>
   );
-};
-
-const getLocation = (): Promise<Location> => {
-  return new Promise<Location>((resolve, reject) => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          resolve({
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          });
-        },
-        (error) => {
-          reject(error);
-        },
-        {
-          enableHighAccuracy: false,
-          maximumAge: 0,
-          timeout: Infinity,
-        },
-      );
-    } else {
-      reject(new Error('Unable to retrieve your location'));
-    }
-  });
 };
 
 Map.defaultProps = {
--- client/src/stories/map.stories.tsx
+++ client/src/stories/map.stories.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Meta } from '@storybook/react/types-6-0';
 import { object, number } from '@storybook/addon-knobs';
-
-import Map from '../components/Map';
+import { Provider } from 'react-redux';
+import MapContainer from '../containers/MapContainer';
+import store from '../stores';
 
 export default {
   title: 'Map/Map',
-  component: Map,
+  component: MapContainer,
 } as Meta;
 
 export const GoogleMap = () => (
-  <Map center={object('위도, 경도', { lat: 37.5006226, lng: 127.0231786 })} zoom={number('줌 레벨', 16)} />
+  <Provider store={store}>
+    <MapContainer />
+  </Provider>
 );
