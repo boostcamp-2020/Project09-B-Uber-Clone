ea8b3975e83cf5b951a0a7d9ebb678c380dce4c5
2hoyeong <leehodud302@naver.com>
2020-11-26 01:10:51 +0900

    [fe] refactor (#12) configure 제거
    스토어를 한개만 사용하면 configure는 필요가 없습니다 제거!

--- client/src/stores/index.ts
+++ client/src/stores/index.ts
@@ -1,2 +1,10 @@
-import configure from './configure';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import modules from './modules';
+
+const configure = () => {
+  const store = createStore(modules, composeWithDevTools());
+  return store;
+};
+
 export default configure();
