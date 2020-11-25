6e849e8e2566e45853cbbd49fa04ab945c837a7c
2hoyeong <44524462+2hoyeong@users.noreply.github.com>
2020-11-25 15:14:24 +0900

    [fe] feat (#12) redux 적용
    react redux를 이용해 store를 모든 곳에서 접근 할 수 있도록 적용했습니다.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,27 +16,31 @@ import {
   DriverSignupPage,
   NotFoundPage,
 } from './pages';
+import { Provider } from 'react-redux';
+import store from './stores';
 
 const App: React.FC = () => {
   return (
-    <ApolloProvider client={client}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={UserDriverSelectPage} />
-          <Route exact path="/user" component={UserAuthPage} />
-          <Route path="/user/signup" component={UserSignupPage} />
-          <Route path="/user/signin" component={UserSigninPage} />
-          <Route path="/user/map" component={UserMainPage} />
-          <Route path="/user/matching" component={UserMatchingPage} />
-          <Route exact path="/driver" component={DriverAuthPage} />
-          <Route path="/driver/signup" component={DriverSignupPage} />
-          <Route path="/driver/signin" component={DriverSigninPage} />
-          <Route path="/driver/map" component={DriverMainPage} />
-          <Route path="/driver/matching" component={DriverMatchingPage} />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </BrowserRouter>
-    </ApolloProvider>
+    <Provider store={store}>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={UserDriverSelectPage} />
+            <Route exact path="/user" component={UserAuthPage} />
+            <Route path="/user/signup" component={UserSignupPage} />
+            <Route path="/user/signin" component={UserSigninPage} />
+            <Route path="/user/map" component={UserMainPage} />
+            <Route path="/user/matching" component={UserMatchingPage} />
+            <Route exact path="/driver" component={DriverAuthPage} />
+            <Route path="/driver/signup" component={DriverSignupPage} />
+            <Route path="/driver/signin" component={DriverSigninPage} />
+            <Route path="/driver/map" component={DriverMainPage} />
+            <Route path="/driver/matching" component={DriverMatchingPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </BrowserRouter>
+      </ApolloProvider>
+    </Provider>
   );
 };
 
