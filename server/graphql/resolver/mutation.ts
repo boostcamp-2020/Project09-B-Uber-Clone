88e7f6fb91f0bd1f3e189495aa2438b2c2b0025f
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-25 15:19:33 +0900

    [be] feat (#19) 드라이버 비밀번호 암호화 - 드라이버 회원가입 시 비밀번호를 bcrypt로 암호화해서 저장

--- server/graphql/resolver/mutation.ts
+++ server/graphql/resolver/mutation.ts
@@ -1,7 +1,11 @@
+import bcrypt from 'bcrypt';
+
 const Mutation = {
   driverSignup: async (_, args, { dataSources, res }) => {
+    const hashedPassword = await bcrypt.hashSync(args.password, process.env.BCRYPT_SALT_ROUNDS);
+
     const driverSchema = dataSources.model('Driver');
-    const newDriver = new driverSchema(args);
+    const newDriver = new driverSchema({ ...args, password: hashedPassword });
     const result = await newDriver.save();
 
     if (!result) return { success: false, message: '회원가입 중 오류가 발생했습니다' };
