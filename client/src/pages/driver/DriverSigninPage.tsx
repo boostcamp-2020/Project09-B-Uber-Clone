82b462c0969685d06730fd66a9df1bf47f3940a1
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-11-26 15:47:54 +0900

    (fe) feat (#6) 드라이버 로그인 페이지 구현 - 드라이버 로그인 mutation 연결 - 로그인 폼 재사용

--- client/src/pages/driver/DriverSigninPage.tsx
+++ client/src/pages/driver/DriverSigninPage.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import LoginForm from '@components/Signin/SigninForm';
+import { useMutation, gql } from '@apollo/client';
+import { useHistory } from 'react-router-dom';
 
+const SIGNIN_DRIVER = gql`
+  mutation DriverSignin($id: String!, $password: String!) {
+    driverSignin(id: $id, password: $password) {
+      success
+      message
+    }
+  }
+`;
 const DriverSigninPage: React.FC = () => {
-  return <div>드라이버 로그인 페이지</div>;
+  const [signinDriver] = useMutation(SIGNIN_DRIVER);
+  const history = useHistory();
+
+  return <LoginForm signin={signinDriver} history={history} />;
 };
 
 export default DriverSigninPage;
