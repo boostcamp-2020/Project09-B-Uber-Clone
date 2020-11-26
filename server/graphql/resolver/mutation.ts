89708a00cab798912b13e4ee825399dc8ea2bb00
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-11-26 23:17:21 +0900

    (be) fix (#5) 오타 수정

--- server/graphql/resolver/mutation.ts
+++ server/graphql/resolver/mutation.ts
@@ -33,7 +33,7 @@ const Mutation = {
       return { success: false, message: '존재하지 않는 아이디입니다.' };
     } catch (err) {
       logger.info('User login error!');
-      return { status: false, message: '유효하지 않은 접근입니다.' };
+      return { success: false, message: '유효하지 않은 접근입니다.' };
     }
   },
   driverSignin: async (_, args, { dataSources, res }) => {
