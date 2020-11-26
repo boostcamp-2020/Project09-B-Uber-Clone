e9f2afc927f11ac3e1b59497fe816fc3a83cd243
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-11-26 15:09:31 +0900

    (fe) feat (#5) 로그인 페이지의 로그인 폼 props 수정 - 로그인 버튼 클릭 이벤트, history를 props로 전달

--- client/src/pages/user/UserSigninPage.tsx
+++ client/src/pages/user/UserSigninPage.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import LoginForm from '../../components/Login/LoginForm';
+import LoginForm from '../../components/Signin/SigninForm';
+import { useMutation, gql } from '@apollo/client';
+import { useHistory } from 'react-router-dom';
+
+const SIGNIN_USER = gql`
+  mutation UserSignin($id: String!, $password: String!) {
+    userSignin(id: $id, password: $password) {
+      success
+      message
+    }
+  }
+`;
 
 const UserSigninPage: React.FC = () => {
-  return <LoginForm />;
+  const [signinUser] = useMutation(SIGNIN_USER);
+  const history = useHistory();
+
+  return <LoginForm signin={signinUser} history={history} />;
 };
 
 export default UserSigninPage;
