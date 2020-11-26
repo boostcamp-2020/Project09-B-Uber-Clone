c70ef9851b9dc540182b88e09c33b7fb5f360bed
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-27 00:53:36 +0900

    [be] refactor (#31) AuthDirective 통합 - 유저와 드라이버 간 인증 코드에 중복이 많아 통합

--- server/graphql/schemaDirectives/index.ts
+++ server/graphql/schemaDirectives/index.ts
@@ -1,15 +1,37 @@
-import { SchemaDirectiveVisitor } from 'apollo-server-express';
+import { SchemaDirectiveVisitor, AuthenticationError } from 'apollo-server-express';
 import { defaultFieldResolver } from 'graphql';
-import { userResolve } from './user';
-import { driverResolve } from './driver';
+import jwt from 'jsonwebtoken';
+import Config from '../../config';
+import { authError } from './authError';
+import { logger } from '../../config/winston';
 
 class AuthDirective extends SchemaDirectiveVisitor {
   visitFieldDefinition(field): any {
     const requiredRole = this.args.requires;
     const originalResolve = field.resolve || defaultFieldResolver;
+    const isDriverAuth = requiredRole === 'DRIVER';
 
-    if (requiredRole === 'USER') field.resolve = userResolve(originalResolve);
-    else if (requiredRole === 'DRIVER') field.resolve = driverResolve(originalResolve);
+    field.resolve = async (...args) => {
+      const { req, dataSources } = args[2];
+      try {
+        const cookie = isDriverAuth ? req.signedCookies.driverToken : req.signedCookies.userToken;
+        if (!cookie) authError();
+
+        const { id } = jwt.verify(cookie, Config.JWT_SECRET);
+        if (!id) authError();
+
+        const modelName = isDriverAuth ? 'Driver' : 'User';
+        const requestSchema = dataSources.model(modelName);
+        const result = await requestSchema.findById(id);
+        if (!result) authError();
+
+        return originalResolve.apply(this, args);
+      } catch (err) {
+        if (err instanceof AuthenticationError) throw err;
+        logger.info('Server Error');
+        throw new Error('오류가 발생했습니다');
+      }
+    };
   }
 }
 
