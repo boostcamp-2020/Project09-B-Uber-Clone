4eb146c540045bb3364a961b1d6dc47771ae3442
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-28 00:32:01 +0900

    [fe] feat (#20) 경로 표시 구현 - effect function으로 pathPoint가 변경될 때마다 확인 - 출발지, 도착지가 모두 선택된 경우 api로 경로를 가져옴 - 해당 경로를 지도에 render하는 callback 작성

--- client/src/components/map/Map.tsx
+++ client/src/components/map/Map.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import GoogleMapReact from 'google-map-react';
 import Marker from '../common/Marker';
 import { Location, Marker as MarkerType, PathPoint } from '@custom-types';
@@ -11,20 +11,55 @@ const Map: React.FC<{
   updateMyLocation: () => void;
   moveCenterMyLocation: () => void;
 }> = ({ center, location, pathPoint, zoom, updateMyLocation, moveCenterMyLocation }) => {
+  const [maps, setMaps] = useState({ map: null });
+
+  const renderDirection: (result: google.maps.DirectionsResult, status: google.maps.DirectionsStatus) => void = (
+    result,
+    status,
+  ) => {
+    if (status === google.maps.DirectionsStatus.OK) {
+      const directionsRenderer = new google.maps.DirectionsRenderer();
+      /**
+       * result.routes[0].legs[0].distance.text => 이동 거리
+       * result.routes[0].legs[0].duration.text => 이동에 필요한 시간
+       */
+      directionsRenderer.setMap(maps.map);
+      directionsRenderer.setDirections(result);
+    }
+  };
+
   useEffect(() => {
     setInterval(updateMyLocation, 1000);
   }, []);
 
+  useEffect(() => {
+    const { startPoint, endPoint } = pathPoint;
+    if (maps && startPoint.lat && endPoint.lat) {
+      const origin: Location = { lat: startPoint.lat as number, lng: startPoint.lng as number };
+      const destination: Location = { lat: endPoint.lat as number, lng: endPoint.lng as number };
+      const DirectionsService = new google.maps.DirectionsService();
+      DirectionsService.route(
+        {
+          origin,
+          destination,
+          travelMode: google.maps.TravelMode.DRIVING,
+        },
+        renderDirection,
+      );
+    }
+  }, [pathPoint]);
+
   return (
     <div style={{ height: '100vh', width: '100%' }}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY || '' }}
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY || '', libraries: ['places'] }}
         defaultZoom={zoom}
         center={center}
         onTilesLoaded={() => {
           updateMyLocation();
           moveCenterMyLocation();
         }}
+        onGoogleApiLoaded={setMaps}
       >
         <Marker lat={location.lat} lng={location.lng} color="#95A5A6" />
         <Marker lat={pathPoint.startPoint.lat} lng={pathPoint.startPoint.lng} color="#4285F4" />
