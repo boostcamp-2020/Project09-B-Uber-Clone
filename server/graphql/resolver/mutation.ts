5faa31bf1b37a00be5bb6a0dcb66ab3579c7f7a4
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-27 11:02:00 +0900

    [be] refactor (#31) mutation 설정 변경 - mutation에서 비밀번호 비교를 compareSync에서 compare로 변경 - 오타 수정 - 쿠키에 httpOnly 속성 추가

--- server/graphql/resolver/mutation.ts
+++ server/graphql/resolver/mutation.ts
@@ -16,7 +16,7 @@ const Mutation = {
     if (!result) return { success: false, message: '회원가입 중 오류가 발생했습니다' };
 
     const token = jwt.sign({ id: result._id }, Config.JWT_SECRET);
-    res.cookie('driverToken', token, { signed: true });
+    res.cookie('driverToken', token, { signed: true, httpOnly: true });
     return { success: true };
   },
   userSignin: async (_, args, { dataSources, res }) => {
@@ -24,7 +24,7 @@ const Mutation = {
       const userSchema = dataSources.model('User');
       const user = await userSchema.findOne({ id: args.id });
       if (user) {
-        if (await bcrypt.compareSync(args.password, user.password)) {
+        if (await bcrypt.compare(args.password, user.password)) {
           const token = jwt.sign({ id: user._id }, Config.JWT_SECRET);
           res.cookie('userToken', token, { httpOnly: true, signed: true });
           logger.info(`${args.id} user logined!`);
@@ -35,7 +35,7 @@ const Mutation = {
       return { success: false, message: '존재하지 않는 아이디입니다.' };
     } catch (err) {
       logger.info('User login error!');
-      return { status: false, message: '유효하지 않은 접근입니다.' };
+      return { success: false, message: '유효하지 않은 접근입니다.' };
     }
   },
   driverSignin: async (_, args, { dataSources, res }) => {
@@ -43,7 +43,7 @@ const Mutation = {
       const driverSchema = dataSources.model('Driver');
       const driver = await driverSchema.findOne({ id: args.id });
       if (driver) {
-        if (await bcrypt.compareSync(args.password, driver.password)) {
+        if (await bcrypt.compare(args.password, driver.password)) {
           const token = jwt.sign({ id: driver._id }, Config.JWT_SECRET);
           res.cookie('driverToken', token, { httpOnly: true, signed: true });
           logger.info(`${args.id} driver logined!`);
