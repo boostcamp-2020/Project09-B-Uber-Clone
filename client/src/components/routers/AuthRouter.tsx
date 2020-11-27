15905b83b121d9d8ae3b157a8df54348f4fcca7e
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-11-27 10:44:18 +0900

    [fe] feat (#31) 인증 불필요 페이지에서 유저 확인 - 인증된 유저는 map 페이지로 이동 - 인증되지 않은 유저에게 회원가입, 로그인 관련 페이지를 보여줌

--- client/src/components/routers/AuthRouter.tsx
+++ client/src/components/routers/AuthRouter.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
-import Cookies from 'js-cookie';
+import { useQuery } from '@apollo/client';
+import styled from 'styled-components';
+import { ActivityIndicator } from 'antd-mobile';
+import { IS_DRIVER_AUTHORIZED, IS_USER_AUTHORIZED } from '@utils/authGQLString';
 
 const AuthRouter: React.FC<any> = ({ component, ...rest }) => {
   const isDriver = rest.path.startsWith('/driver');
-  const hasCookie = isDriver ? !!Cookies.get('driverToken') : !!Cookies.get('userToken');
+  const { loading, error, data } = useQuery(isDriver ? IS_DRIVER_AUTHORIZED : IS_USER_AUTHORIZED);
 
-  return hasCookie ? (
-    <Redirect to={`/${isDriver ? 'driver' : 'user'}/map`} />
-  ) : (
-    <Route {...rest} render={(props) => React.createElement(component, props)} />
-  );
+  if (loading)
+    return (
+      <Wrapper>
+        <ActivityIndicator animating text="로딩 중입니다" />
+      </Wrapper>
+    );
+  if (error || !data.isAuthorizedDriver)
+    return <Route {...rest} render={(props) => React.createElement(component, props)} />;
+  return <Redirect to={`/${isDriver ? 'driver' : 'user'}/map`} />;
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+`;
 
 export default AuthRouter;
