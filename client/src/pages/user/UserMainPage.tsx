9dac91057c19871b0a5ba12cf1cca22d79d46598
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-11-29 18:46:55 +0900

    [fe] feat (#17) 출발/도착 입력지 설정 시 핀 표시하기 - 출발/도착 입력 탭 각각에 대한 이벤트핸들러를 구현

--- client/src/pages/user/UserMainPage.tsx
+++ client/src/pages/user/UserMainPage.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import MapContainer from '../../containers/MapContainer';
+import { updateStartPoint, updateEndPoint } from '../../stores/modules/pathPoint';
+import { useDispatch } from 'react-redux';
+import { LatLng } from 'react-google-places-autocomplete/build/GooglePlacesAutocomplete.types';
 
 const UserMainPage: React.FC = () => {
-  return <MapContainer />;
+  const dispatch = useDispatch();
+
+  const selectStartHandler = (placeName: string, latLng: LatLng) => {
+    dispatch(updateStartPoint(latLng));
+  };
+  const selectEndHandler = (placeName: string, latLng: LatLng) => {
+    dispatch(updateEndPoint(latLng));
+  };
+
+  return (
+    // Todo: 출발-도착지 입력 탭 컴포넌트
+    // 출발 입력 탭 selectHandler={selectStartHandler}
+    // 도착 입력 탭 selectHandler={selectEndHandler}
+    <MapContainer />
+  );
 };
 
 export default UserMainPage;
