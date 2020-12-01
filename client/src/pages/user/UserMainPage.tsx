1f4500c76a947ac1ac7bdd4657476016faee30b3
2hoyeong <leehodud302@naver.com>
2020-12-01 14:23:53 +0900

    [fe] fix (#10) 자동완성 작동 안하던 점 수정
    자동완성이 Map 컴포넌트 밖으로 빠지면서 api script 로더가 정상적으로 되지 않아 발생하던 오류 수정했습니다.

--- client/src/pages/user/UserMainPage.tsx
+++ client/src/pages/user/UserMainPage.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MapContainer from '../../containers/MapContainer';
 import InputPlaces from '@components/userMain/InputPlaces';
+import { Loader } from '@googlemaps/js-api-loader';
+
+const loader = new Loader({
+  apiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY || '',
+  libraries: ['places'],
+});
 
 const UserMainPage: React.FC = () => {
+  const [apiLoaded, setApiLoaded] = useState(false);
+
+  const initialScriptLoad = async () => {
+    await loader.load();
+    setApiLoaded(true);
+  };
+
+  useEffect(() => {
+    initialScriptLoad();
+  }, []);
+
   return (
     <>
-      <InputPlaces />
-      <MapContainer />;
+      {apiLoaded && (
+        <>
+          <InputPlaces />
+          <MapContainer />;
+        </>
+      )}
     </>
   );
 };
