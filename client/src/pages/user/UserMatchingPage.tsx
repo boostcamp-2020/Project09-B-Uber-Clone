d57c62973fe04a495ddef5b1e3340f9ab3bd7600
JungSWon <sw.onandon@gmail.com>
2020-12-01 20:59:39 +0900

    [fe] feat (#50) `MapContainer` 포함하는 `UserMatchingPage`

--- client/src/pages/user/UserMatchingPage.tsx
+++ client/src/pages/user/UserMatchingPage.tsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Loader } from '@googlemaps/js-api-loader';
 import MatchedDriverData from '@components/UserMatching/MatchedDriverData';
+import MapContainer from '../../containers/MapContainer';
+
+const loader = new Loader({
+  apiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY || '',
+  libraries: ['places'],
+});
+
 const UserMatchingPage: React.FC = () => {
+  const [apiLoaded, setApiLoaded] = useState(false);
+
+  const initialScriptLoad = async () => {
+    await loader.load();
+    setApiLoaded(true);
+  };
+
+  useEffect(() => {
+    initialScriptLoad();
+  }, []);
+
   // TODO : 매칭이 성사되면(드라이버 수락 후) MatchedDriverData 노출
-  return <MatchedDriverData />;
+  return (
+    <>
+      {apiLoaded && (
+        <>
+          <MapContainer />
+          <MatchedDriverData />
+        </>
+      )}
+    </>
+  );
 };
 
 export default UserMatchingPage;
