58abba24d76c33496863e4170155315ed8a651da
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-12-01 18:24:04 +0900

    [fe] feat (#64) 드라이버 영업 시작 및 종료 구현 - 영업 시작 시 service 시작 - location을 2초마다 업데이트 - cleanup 함수로 stopService와 interval clear

--- client/src/components/driverMain/DriverMainBody.tsx
+++ client/src/components/driverMain/DriverMainBody.tsx
@@ -1,8 +1,53 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { gql, useMutation } from '@apollo/client';
+import getLocation from '@utils/getLocation';
 import styled from 'styled-components';
 import DriverWorkingContent from './DriverWorkingContent';
 
+const START_SERVICE = gql`
+  mutation {
+    startService {
+      success
+      message
+    }
+  }
+`;
+const STOP_SERVICE = gql`
+  mutation {
+    stopService {
+      success
+      message
+    }
+  }
+`;
+const UPDATE_LOCATION = gql`
+  mutation UpdateDriverLocation($location: LatLngInput) {
+    updateDriverLocation(location: $location) {
+      success
+      message
+    }
+  }
+`;
+
 const DriverMainBody: React.FC<{ isWorking: boolean }> = ({ isWorking }) => {
+  const [updateDriverLocation] = useMutation(UPDATE_LOCATION);
+  const [startService] = useMutation(START_SERVICE);
+  const [stopService] = useMutation(STOP_SERVICE);
+
+  useEffect(() => {
+    if (isWorking) {
+      startService();
+      const updateLocation = setInterval(async () => {
+        const location = await getLocation();
+        updateDriverLocation({ variables: { location } });
+      }, 2000);
+      return () => {
+        clearInterval(updateLocation);
+        stopService();
+      };
+    }
+  }, [isWorking]);
+
   return <Wrapper>{isWorking ? <DriverWorkingContent /> : <WorkingFinished>영업종료</WorkingFinished>}</Wrapper>;
 };
 
