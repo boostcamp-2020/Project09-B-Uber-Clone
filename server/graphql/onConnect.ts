f68367c87bedb7e81cbdcf7c39065611a044cd8c
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-12-02 01:49:04 +0900

    [be] feat (#66) graphql 설정 변경 - 요청시간 대신 만료 시간을 보내도록 설정 - 드라이버 DB 스키마가 변경되어 id 검증 로직 수정 - 쿠키 unsign 로직 변경

--- server/graphql/onConnect.ts
+++ server/graphql/onConnect.ts
@@ -1,4 +1,5 @@
 import Models from '../models';
+import cookie from 'cookie';
 import cookieParser from 'cookie-parser';
 import { AuthenticationError } from 'apollo-server-express';
 import Config from '../config';
@@ -7,14 +8,8 @@ export const onConnect = (_, webSocket) => {
   const requestCookie = webSocket.upgradeReq.headers.cookie;
   if (!requestCookie) throw new AuthenticationError('유효하지 않은 사용자입니다');
 
-  const tokens = requestCookie.split(';');
-  const cookies = tokens.reduce((acc: any, token: string) => {
-    if (token.trim().startsWith('userToken=s%3A')) acc.userToken = token.split('=')[1].trim().replace('%3A', ':');
-    else if (token.trim().startsWith('driverToken=s%3A'))
-      acc.driverToken = token.split('=')[1].trim().replace('%3A', ':');
-    return acc;
-  }, {});
-
+  const parsedCookie = cookie.parse(requestCookie);
+  const cookies = cookieParser.JSONCookies(parsedCookie);
   const decoded = cookieParser.signedCookies(cookies, Config.COOKIE_SECRET);
   if (!decoded.userToken && !decoded.driverToken) throw new AuthenticationError('유효하지 않은 사용자입니다');
 
