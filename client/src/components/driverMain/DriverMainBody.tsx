9223a351c03a0e3deb7503144d9b18e98ceb1f3e
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-12-02 01:50:55 +0900

    [fe] feat (#66) Driver mutation 처리 - 에러 시 toast 발생 후 auth 페이지로 리디렉트하도록 함 - 로딩중일 경우 로딩중 창을 보여줌

--- client/src/components/driverMain/DriverMainBody.tsx
+++ client/src/components/driverMain/DriverMainBody.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { gql, useMutation } from '@apollo/client';
 import getLocation from '@utils/getLocation';
 import styled from 'styled-components';
+import { ActivityIndicator, Toast } from 'antd-mobile';
 import DriverWorkingContent from './DriverWorkingContent';
 
 const START_SERVICE = gql`
@@ -30,9 +32,19 @@ const UPDATE_LOCATION = gql`
 `;
 
 const DriverMainBody: React.FC<{ isWorking: boolean }> = ({ isWorking }) => {
+  const history = useHistory();
   const [updateDriverLocation] = useMutation(UPDATE_LOCATION);
-  const [startService] = useMutation(START_SERVICE);
-  const [stopService] = useMutation(STOP_SERVICE);
+  const [startService, { loading: updateStartServiceLoading, error: updateStartServiceError }] = useMutation(
+    START_SERVICE,
+  );
+  const [stopService, { loading: updateStopServiceLoading, error: updateStopServiceError }] = useMutation(STOP_SERVICE);
+
+  useEffect(() => {
+    if (updateStartServiceError || updateStopServiceError)
+      Toast.fail('오류가 발생했습니다. 드라이버 로그인 페이지로 이동합니다.', 3, () => {
+        history.push('/driver');
+      });
+  }, [updateStartServiceError, updateStopServiceError]);
 
   useEffect(() => {
     if (isWorking) {
@@ -48,10 +60,17 @@ const DriverMainBody: React.FC<{ isWorking: boolean }> = ({ isWorking }) => {
     }
   }, [isWorking]);
 
-  return <Wrapper>{isWorking ? <DriverWorkingContent /> : <WorkingFinished>영업종료</WorkingFinished>}</Wrapper>;
+  return (
+    <Wrapper>
+      {(updateStartServiceLoading || updateStopServiceLoading) && <ActivityIndicator toast text="로딩중..." />}
+      <>{isWorking ? <DriverWorkingContent /> : <WorkingFinished>영업종료</WorkingFinished>}</>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 100%;
   height: 100%;
   padding: 50px;
