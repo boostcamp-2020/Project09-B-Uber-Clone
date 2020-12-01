452fec15119e8e336a97e102b041b29b31a642b5
2hoyeong <leehodud302@naver.com>
2020-12-02 01:14:33 +0900

    [fe] feat (#55) 매칭 요청하기 버튼 기능 구현
    매칭 요청하기 버튼을 누르면 Mutation으로 매칭정보를 전송해 저장할 수 있도록 구현했습니다.

--- client/src/components/userMain/RequsetButton.tsx
+++ client/src/components/userMain/RequsetButton.tsx
@@ -1,17 +1,67 @@
 import React from 'react';
-import { Button } from 'antd-mobile';
+import { Button, Toast } from 'antd-mobile';
 import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { PathPoint } from '@custom-types';
+import { useMutation, gql } from '@apollo/client';
 
 const MatchingReqBtn: React.FC = () => {
+  const pathPoint = useSelector((state: { pathPoint: PathPoint }) => state.pathPoint);
   const history = useHistory();
+  const [requestMatch] = useMutation(REQUEST_MATCH);
+
   const handlClick = async () => {
-    history.push('/user/matching');
+    const [result, message] = await registMatchingList();
+    if (result) await history.push('/user/matching');
+    else Toast.show(message, Toast.LONG);
   };
+
+  const registMatchingList = async () => {
+    if (!pathPoint.isSetStartPoint || !pathPoint.isSetEndPoint) return [false, '출발지와 도착지를 입력하세요'];
+    const request = {
+      startLocation: {
+        name: pathPoint.startGeocode?.PointName,
+        latlng: {
+          ...pathPoint.startPoint,
+        },
+      },
+      endLocation: {
+        name: pathPoint.endGeocode?.PointName,
+        latlng: {
+          ...pathPoint.endPoint,
+        },
+      },
+    };
+
+    const result = false;
+    const message = '';
+    try {
+      const {
+        data: {
+          requestMatching: { success: result, message: message },
+        },
+      } = await requestMatch({ variables: { request } });
+      return [result, message];
+    } catch (error) {
+      console.error(error);
+      return [result, message];
+    }
+  };
+
   return (
-    <Button onClick={handlClick} type="primary">
+    <Button onClick={handlClick} type="primary" disabled={!pathPoint.isSetStartPoint || !pathPoint.isSetEndPoint}>
       택시자버 요청하기
     </Button>
   );
 };
+
+const REQUEST_MATCH = gql`
+  mutation requestMatching($request: UserRequestInput) {
+    requestMatching(request: $request) {
+      success
+      message
+    }
+  }
+`;
 
 export default MatchingReqBtn;
