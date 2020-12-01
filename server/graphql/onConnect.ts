268f82a38f0de426b801b50fd365a1396194ef3e
2hoyeong <leehodud302@naver.com>
2020-12-01 16:52:49 +0900

    [be] fix (#55) 쿠키가 정상적으로 decode되지 않던 버그 수정
    쿠키가 정상적으로 파싱후 decode되지 않던 버그를 수정했습니다.

--- server/graphql/onConnect.ts
+++ server/graphql/onConnect.ts
@@ -1,20 +1,16 @@
 import Models from '../models';
+import cookie from 'cookie';
 import cookieParser from 'cookie-parser';
 import { AuthenticationError } from 'apollo-server-express';
 import Config from '../config';
 
 export const onConnect = (_, webSocket) => {
   const requestCookie = webSocket.upgradeReq.headers.cookie;
-  if (!requestCookie) throw new AuthenticationError('유효하지 않은 사용자입니다');
-
-  const tokens = requestCookie.split(';');
-  const cookies = tokens.reduce((acc: any, token: string) => {
-    if (token.trim().startsWith('userToken=s%3A')) acc.userToken = token.split('=')[1].trim().replace('%3A', ':');
-    else if (token.trim().startsWith('driverToken=s%3A'))
-      acc.driverToken = token.split('=')[1].trim().replace('%3A', ':');
-    return acc;
-  }, {});
-
+  if (!requestCookie) {
+    throw new AuthenticationError('유효하지 않은 사용자입니다');
+  }
+  const parsedCookie = cookie.parse(requestCookie);
+  const cookies = cookieParser.JSONCookies(parsedCookie);
   const decoded = cookieParser.signedCookies(cookies, Config.COOKIE_SECRET);
   if (!decoded.userToken && !decoded.driverToken) throw new AuthenticationError('유효하지 않은 사용자입니다');
 
