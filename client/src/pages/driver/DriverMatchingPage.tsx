d1868f147dd62843d7ae8489929a094b5c6741be
JungSWon <sw.onandon@gmail.com>
2020-12-03 02:06:06 +0900

    [fe] feat (#69) 승객 출발지 표시 - `subscription` 으로 유저의 요청 데이터를 가져옴 - 데이터를 `StartLocationInfo` 컴포넌트로 전달

--- client/src/pages/driver/DriverMatchingPage.tsx
+++ client/src/pages/driver/DriverMatchingPage.tsx
@@ -1,17 +1,71 @@
+import React, { useState, useEffect } from 'react';
 import React from 'react';
+import { gql, useSubscription } from '@apollo/client';
+import { Loader } from '@googlemaps/js-api-loader';
+import { Toast } from 'antd-mobile';
 import MapContainer from '../../containers/MapContainer';
 import CallButton from '@components/common/CallButton';
 import StartLocationInfo from '@components/driverMatching/StartLocationInfo';
 import styled from 'styled-components';
 
+const loader = new Loader({
+  apiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY || '',
+  libraries: ['places'],
+});
+
+const MATCHED_USER = gql`
+  subscription {
+    driverServiceSub {
+      uid
+      request {
+        startLocation
+        endLocation
+      }
+    }
+  }
+`;
+
 const DriverMatchingPage: React.FC = () => {
+  const { data, error } = useSubscription(MATCHED_USER);
+  const [googleMapApi, setGoogleMapApi]: any = useState({ loaded: false, directionRenderer: null });
+  const [userRequest, setUserRequest] = useState({
+    uid: '',
+    startLocation: { name: '', Latlng: { lat: '', lng: '' } },
+    endLocation: { name: '', Latlng: { lat: '', lng: '' } },
+  });
+
+  const initialScriptLoad = async () => {
+    await loader.load();
+    setGoogleMapApi({ loaded: true, directionRenderer: new google.maps.DirectionsRenderer() });
+  };
+
+  useEffect(() => {
+    initialScriptLoad();
+  }, []);
+
+  useEffect(() => {
+    if (data?.driverServiceSub) {
+      const requsetData = data.userMatchingSub;
+      const { startLocation, endLocation } = requsetData.request;
+      setUserRequest({ uid: requsetData.uid, startLocation: startLocation, endLocation: endLocation });
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (error) Toast.fail('유저 정보를 확인할 수 없습니다.');
+  }, [error]);
+
   return (
     <>
-      <MapContainer />
-      <Overlay>
-        <StartLocationInfo startLocation="서울아산병원" />
-        <CallButton phone="010-0000-0000" />
-      </Overlay>
+      {googleMapApi.loaded && (
+        <>
+          <MapContainer directionRenderer={googleMapApi.directionRenderer} />
+          <Overlay>
+            <StartLocationInfo startLocation={userRequest.startLocation.name} />
+            <CallButton phone="010-0000-0000" />
+          </Overlay>
+        </>
+      )}
     </>
   );
 };
