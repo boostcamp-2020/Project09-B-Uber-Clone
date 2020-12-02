035a4d1804bd1e9fe0afef28330089beb76f5723
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-12-02 15:28:33 +0900

    [fe] feat (#46) pathPoint 타입 변경 - 출발지와 도착지의 이름 타입 추가

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -55,10 +55,20 @@ export interface PathPoint {
   isSetEndPoint: boolean;
   startPoint: Marker;
   endPoint: Marker;
+  startPointName: string | undefined;
+  endPointName: string | undefined;
 }
 
 export interface LoginFormPropsType {
   signin: any;
   history: any;
   userType: string;
+}
+export interface Info {
+  time: string;
+  fee: number;
+}
+export interface PreData {
+  isSetPath: boolean;
+  info: Info;
 }
