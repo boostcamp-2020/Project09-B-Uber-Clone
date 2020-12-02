dec520d1c51d0d92df1fb9c96a7d98bf3a205ee4
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-12-02 22:16:44 +0900

    [fe] feat (#49) 승객 탑승 완료 버튼 구현 - useState를 사용하여 탑승 상태를 관리 - 탑승 상태에 따라 지도 위에 표시되는 컴포넌트가 달라짐

--- client/src/pages/driver/DriverMatchingPage.tsx
+++ client/src/pages/driver/DriverMatchingPage.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MapContainer from '../../containers/MapContainer';
 import CallButton from '@components/common/CallButton';
 import styled from 'styled-components';
+import { Button } from 'antd-mobile';
 
 const DriverMatchingPage: React.FC = () => {
+  const [state, setState] = useState(false);
+
   return (
     <>
       <MapContainer />
-      <Overlay>
-        <CallButton phone="010-0000-0000" />
-      </Overlay>
+      {state ? (
+        <> // TODO: 승객 탑승 후 컴포넌트</>
+      ) : (
+        <>
+          <TopOverlay>
+            <CallButton phone="010-0000-0000" />
+          </TopOverlay>
+          <BottomOverlay>
+            <Button type="primary" onClick={() => setState(true)}>
+              승객 탑승 완료
+            </Button>
+          </BottomOverlay>
+        </>
+      )}
     </>
   );
 };
 
-const Overlay = styled.div`
+const TopOverlay = styled.div`
   position: absolute;
   top: 0;
   left: 0;
   right: 0;
+  margin: 10px;
+`;
+
+const BottomOverlay = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: 10px;
 `;
 
 export default DriverMatchingPage;
