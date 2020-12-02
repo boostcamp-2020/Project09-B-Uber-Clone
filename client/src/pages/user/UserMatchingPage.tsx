09f4380646c3943cadd80377e2ed844549316a00
JungSWon <sw.onandon@gmail.com>
2020-12-02 14:02:43 +0900

    [fe] feat (#51) 매칭된 드라이버의 정보를 요청하는 subscription 작성 - 서버 작업이 완료되면 `data` 형식을 보고 `props`로 넘길 예정

--- client/src/pages/user/UserMatchingPage.tsx
+++ client/src/pages/user/UserMatchingPage.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { gql, useSubscription } from '@apollo/client';
 import { Loader } from '@googlemaps/js-api-loader';
 import MatchedDriverData from '@components/UserMatching/MatchedDriverData';
 import MapContainer from '../../containers/MapContainer';
@@ -8,8 +9,23 @@ const loader = new Loader({
   libraries: ['places'],
 });
 
+const MATCHED_TAXI = gql`
+  subscription {
+    userMatchingSub {
+      id
+      name
+      carModel
+      carColor
+      plateNumber
+    }
+  }
+`;
+
 const UserMatchingPage: React.FC = () => {
+  const { data, error } = useSubscription(MATCHED_TAXI);
   const [apiLoaded, setApiLoaded] = useState(false);
+  const [isMatched, setMatchState] = useState(false);
+  const [taxiInfo, setTaxiInfo] = useState(undefined);
 
   const initialScriptLoad = async () => {
     await loader.load();
@@ -20,13 +36,20 @@ const UserMatchingPage: React.FC = () => {
     initialScriptLoad();
   }, []);
 
+  useEffect(() => {
+    if (data?.userMatchingSub) {
+      setMatchState(true);
+      // setTaxiInfo(data);
+    }
+  }, [data]);
+
   // TODO : 매칭이 성사되면(드라이버 수락 후) MatchedDriverData 노출
   return (
     <>
       {apiLoaded && (
         <>
           <MapContainer />
-          <MatchedDriverData />
+          {isMatched && <MatchedDriverData />}
         </>
       )}
     </>
