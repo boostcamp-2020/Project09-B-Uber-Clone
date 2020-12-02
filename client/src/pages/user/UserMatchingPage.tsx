ac515e1b20b6d58bb2cca57a89de593712dfd3e4
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-12-02 16:08:40 +0900

    [fe] feat (#46) userMatching 페이지 지도 표시

--- client/src/pages/user/UserMatchingPage.tsx
+++ client/src/pages/user/UserMatchingPage.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Loader } from '@googlemaps/js-api-loader';
 import MapContainer from 'src/containers/MapContainer';
 import Matching from '@components/userMatching/Matching';
 
+const loader = new Loader({
+  apiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY || '',
+  libraries: ['places'],
+});
+
 const UserMatchingPage: React.FC = () => {
+  const [googleMapApi, setGoogleMapApi]: any = useState({ loaded: false, directionRenderer: null });
+
+  const initialScriptLoad = async () => {
+    await loader.load();
+    setGoogleMapApi({ loaded: true, directionRenderer: new google.maps.DirectionsRenderer() });
+  };
+
+  useEffect(() => {
+    initialScriptLoad();
+  }, []);
+
   return (
     <>
       <Matching />
-      <MapContainer />
+      <MapContainer directionRenderer={googleMapApi.directionRenderer} />
     </>
   );
 };
