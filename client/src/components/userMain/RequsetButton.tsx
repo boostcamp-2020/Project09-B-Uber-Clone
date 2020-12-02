36810bfd28645645d385e3777eea58ef3893599c
2hoyeong <leehodud302@naver.com>
2020-12-02 16:36:13 +0900

    [fe] feat (#56) 매칭 요청 10초마다 보내도록 구현
    매칭 요청을 10초마다 보내고 userMatchSub을 subscription 합니다. 매칭 요청 최대 횟수를 초과하면 map 페이지로 다시 되돌려 보냅니다.

--- client/src/components/userMain/RequsetButton.tsx
+++ client/src/components/userMain/RequsetButton.tsx
@@ -3,49 +3,17 @@ import { Button, Toast } from 'antd-mobile';
 import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { PathPoint } from '@custom-types';
-import { useMutation, gql } from '@apollo/client';
 
 const MatchingReqBtn: React.FC = () => {
-  const pathPoint = useSelector((state: { pathPoint: PathPoint }) => state.pathPoint);
   const history = useHistory();
-  const [requestMatch] = useMutation(REQUEST_MATCH);
+  const pathPoint = useSelector((state: { pathPoint: PathPoint }) => state.pathPoint);
 
   const handlClick = async () => {
-    const [result, message] = await registMatchingList();
-    if (result) await history.push('/user/matching');
-    else Toast.show(message, Toast.LONG);
-  };
-
-  const registMatchingList = async () => {
-    if (!pathPoint.isSetStartPoint || !pathPoint.isSetEndPoint) return [false, '출발지와 도착지를 입력하세요'];
-    const request = {
-      startLocation: {
-        name: pathPoint.startGeocode?.PointName,
-        latlng: {
-          ...pathPoint.startPoint,
-        },
-      },
-      endLocation: {
-        name: pathPoint.endGeocode?.PointName,
-        latlng: {
-          ...pathPoint.endPoint,
-        },
-      },
-    };
-
-    const result = false;
-    const message = '';
-    try {
-      const {
-        data: {
-          requestMatching: { success: result, message: message },
-        },
-      } = await requestMatch({ variables: { request } });
-      return [result, message];
-    } catch (error) {
-      console.error(error);
-      return [result, message];
-    }
+    // const [result, message] = await registMatchingList();
+    // if (result) await history.push('/user/matching');
+    // else Toast.show(message, Toast.LONG);
+    if (!pathPoint.isSetStartPoint || !pathPoint.isSetEndPoint) return;
+    history.push('/user/matching');
   };
 
   return (
@@ -54,14 +22,5 @@ const MatchingReqBtn: React.FC = () => {
     </Button>
   );
 };
-
-const REQUEST_MATCH = gql`
-  mutation requestMatching($request: UserRequestInput) {
-    requestMatching(request: $request) {
-      success
-      message
-    }
-  }
-`;
 
 export default MatchingReqBtn;
--- client/src/pages/user/UserMatchingPage.tsx
+++ client/src/pages/user/UserMatchingPage.tsx
@@ -1,14 +1,103 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MapContainer from 'src/containers/MapContainer';
 import Matching from '@components/userMatching/Matching';
+import { gql, useSubscription, useMutation } from '@apollo/client';
+import { useHistory } from 'react-router-dom';
+import { Toast } from 'antd-mobile';
+import { useSelector } from 'react-redux';
+import { PathPoint } from '@custom-types';
+
+const MAX_REQUEST_COUNT = 6;
+const MATCHING_INTERVAL = 10000;
 
 const UserMatchingPage: React.FC = () => {
+  const pathPoint = useSelector((state: { pathPoint: PathPoint }) => state.pathPoint);
+  const [requestCount, setRequestCount] = useState(MAX_REQUEST_COUNT);
+  const history = useHistory();
+  const [requestMatch] = useMutation(REQUEST_MATCH);
+
+  useEffect(() => {
+    const countdown = setInterval(async () => {
+      await registMatchingList();
+      setRequestCount(requestCount - 1);
+    }, MATCHING_INTERVAL);
+    return () => {
+      if (requestCount === 0) {
+        history.push('/user/map');
+        Toast.show('매칭할 수 있는 드라이버가 없습니다.', Toast.LONG);
+        clearInterval(countdown);
+      }
+    };
+  }, [requestCount]);
+
+  const registMatchingList = async () => {
+    const request = {
+      startLocation: {
+        name: pathPoint.startPointName,
+        latlng: {
+          ...pathPoint.startPoint,
+        },
+      },
+      endLocation: {
+        name: pathPoint.startPointName,
+        latlng: {
+          ...pathPoint.endPoint,
+        },
+      },
+    };
+
+    try {
+      const {
+        data: {
+          requestMatching: { success, message },
+        },
+      } = await requestMatch({ variables: { request } });
+      return [success, message];
+    } catch (error) {
+      console.error(error);
+      return [false, '알 수 없는 오류가 발생했습니다.'];
+    }
+  };
+
+  const subscription = () => {
+    const { loading, error, data } = useSubscription(MATCHING_SUBSCRIPTION);
+    if (loading) return <Matching />;
+    if (error) {
+      Toast.show('알 수 없는 오류가 발생했습니다.');
+      history.push('/user/map');
+      return;
+    }
+    console.log(data);
+    return <p>매칭 성공</p>;
+  };
+
   return (
     <>
-      <Matching />
+      {subscription()}
       <MapContainer />
     </>
   );
 };
+
+const REQUEST_MATCH = gql`
+  mutation requestMatching($request: UserRequestInput) {
+    requestMatching(request: $request) {
+      success
+      message
+    }
+  }
+`;
+
+const MATCHING_SUBSCRIPTION = gql`
+  subscription {
+    userMatchingSub {
+      id
+      name
+      carModel
+      carColor
+      plateNumber
+    }
+  }
+`;
 
 export default UserMatchingPage;
