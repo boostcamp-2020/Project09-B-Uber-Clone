e3d5b3bff8dcc1614535102afbc4c4b30b38ce13
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-12-07 23:45:08 +0900

    [common] feat (#104) 유저 히스토리 페이지 구현 - 조회 query 추가 - useQuery로 해당 유저의 내역 조회 - 결과를 HistoryBox에 매핑 - 매핑을 위해 key값이 필요하기 때문에 응답에 id 추가

--- client/src/pages/user/UserHistoryPage.tsx
+++ client/src/pages/user/UserHistoryPage.tsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import HistoryBox from '@components/userHistory/HistoryBox';
+import { GET_USER_HISTORY } from '@queries/user/userHistory';
+import styled from 'styled-components';
+import { Button } from 'antd';
+import { HomeFilled } from '@ant-design/icons';
 
 const UserHistoryPage: React.FC = () => {
-  return <div>유저 히스토리 페이지</div>;
+  const history = useHistory();
+  const [page, setPage] = useState(1);
+  const { loading, data } = useQuery(GET_USER_HISTORY, { variables: { page } });
+
+  if (loading || !data) return <>로딩중</>;
+  return (
+    <Wrapper>
+      <Title>택시자버 이용내역</Title>
+      {data.userHistory.map(({ id, request, ...info }: any) => (
+        <HistoryBox key={id} {...request} {...info} />
+      ))}
+      <Button
+        shape="circle"
+        icon={<HomeFilled />}
+        size="large"
+        type="primary"
+        style={{ position: 'fixed', bottom: '0', right: '0', margin: '25px' }}
+        onClick={() => {
+          history.push('/user/map');
+        }}
+      />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Title = styled.h1`
+  margin: 25px 0 10px 25px;
+`;
 
 export default UserHistoryPage;
