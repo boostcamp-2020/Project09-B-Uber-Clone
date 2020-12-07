02d2381d3404b336f082f1ccd103b5c2f3575735
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-12-07 22:43:06 +0900

    [be] feat (#104) schema에 내역 조회 query 추가 - Query type에 내역 조회 query 추가 - return type 정의 - resolver 내 boilerplate 생성

--- server/graphql/resolver/query.ts
+++ server/graphql/resolver/query.ts
@@ -1,6 +1,9 @@
 const Query = {
   isAuthorizedUser: () => true,
   isAuthorizedDriver: () => true,
+  userHistory: async (_, { page }, { dataSources, uid }) => {
+    // TODO: history 조회 결과 return
+  },
 };
 
 export default Query;
