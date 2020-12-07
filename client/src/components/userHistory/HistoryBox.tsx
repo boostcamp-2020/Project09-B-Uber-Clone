a559d768a37562db65af26cf91e7a9398e30f713
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-12-07 18:55:38 +0900

    [fe] feat (#104) history 컴포넌트 추가 - antd-mobile List, Item 컴포넌트로 내역 컴포넌트 구현 - 장소와 시간, 요금, 차량 정보를 props로 받음   - 이를 위한 types 정의

---
+++ client/src/components/userHistory/HistoryBox.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import moment from 'moment';
+import { Card, WhiteSpace, List } from 'antd-mobile';
+import { LocationWithName } from '@custom-types';
+import styled from 'styled-components';
+import 'antd-mobile/lib/card/style/css';
+import 'antd-mobile/lib/white-space/style/css';
+import 'antd-mobile/lib/list/style/css';
+
+const Item = List.Item;
+
+const HistoryBox: React.FC<HistoryType> = ({
+  startLocation,
+  endLocation,
+  startTime,
+  endTime,
+  fee,
+  carModel,
+  plateNumber,
+}) => {
+  return (
+    <Wrapper>
+      <Card full>
+        <Card.Body>
+          <List renderHeader={() => moment(startTime).format('YYYY/MM/DD')}>
+            <Item extra={startLocation} wrap>
+              출발
+            </Item>
+            <Item extra={endLocation} wrap>
+              도착
+            </Item>
+            <Item extra={`${moment(startTime).format('HH:mm')} - ${moment(endTime).format('HH:mm')}`} wrap>
+              운행 시간
+            </Item>
+            <Item extra={`${fee}원`}>요금</Item>
+            <Item extra={`${carModel} | ${plateNumber}`} wrap>
+              택시 정보
+            </Item>
+          </List>
+        </Card.Body>
+      </Card>
+      <WhiteSpace size="lg" />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  .am-card-body {
+    padding: 6px 15px;
+  }
+  .am-list-item {
+    min-height: 0.8rem !important;
+    div {
+      font-size: 0.9rem !important;
+      border-bottom: none !important;
+    }
+    .am-list-content {
+      flex: 1 0 !important;
+      color: gray;
+    }
+    .am-list-extra {
+      flex: 2 1 !important;
+      color: black;
+      text-align: left;
+    }
+  }
+`;
+
+interface HistoryType {
+  startLocation: LocationWithName;
+  endLocation: LocationWithName;
+  startTime: Date;
+  endTime: Date;
+  fee: number;
+  carModel: string;
+  plateNumber: string;
+}
+
+export default HistoryBox;
