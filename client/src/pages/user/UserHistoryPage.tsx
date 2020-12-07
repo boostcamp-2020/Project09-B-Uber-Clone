643ce75053e007da9ecbd2b1759d54b46e2d656b
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-12-08 01:07:12 +0900

    [fe] feat (#104) 내역 조회 페이지 구현 - data를 기반으로 내역을 보여줌   - 페이지 당 보여줄 내역 개수를 변수로 설정 - 페이지네이션 적용 - 내역이 없을 경우 내역이 없다고 보여줌

--- client/src/pages/user/UserHistoryPage.tsx
+++ client/src/pages/user/UserHistoryPage.tsx
@@ -1,24 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import HistoryBox from '@components/userHistory/HistoryBox';
 import { GET_USER_HISTORY } from '@queries/user/userHistory';
 import styled from 'styled-components';
 import { Button } from 'antd';
+import { ActivityIndicator } from 'antd-mobile';
 import { HomeFilled } from '@ant-design/icons';
+
+const RESULT_PER_PAGE = 10;
 
 const UserHistoryPage: React.FC = () => {
   const history = useHistory();
   const [page, setPage] = useState(1);
-  const { loading, data } = useQuery(GET_USER_HISTORY, { variables: { page } });
+  const [buttonDisabled, setButtonDisabled] = useState({ prev: true, next: false });
+  const [showingHistory, setShowingHistory] = useState([]);
+  const { loading, data } = useQuery(GET_USER_HISTORY, { variables: { page: page }, fetchPolicy: 'network-only' });
 
-  if (loading || !data) return <>로딩중</>;
+  const onPrev = useCallback(() => {
+    setPage(page - 1);
+    if (page === 2) setButtonDisabled({ prev: true, next: false });
+    else setButtonDisabled({ ...buttonDisabled, next: false });
+  }, [page, buttonDisabled]);
+
+  const onNext = useCallback(() => {
+    setPage(page + 1);
+    setButtonDisabled({ ...buttonDisabled, prev: false });
+  }, [page, buttonDisabled]);
+
+  useEffect(() => {
+    if (data?.userHistory.length) {
+      setShowingHistory(data.userHistory);
+      if (data.userHistory.length < RESULT_PER_PAGE) setButtonDisabled({ ...buttonDisabled, next: true });
+    } else if (data?.userHistory.length === 0) {
+      setPage(page - 1);
+      setButtonDisabled({ ...buttonDisabled, next: true });
+    }
+  }, [data]);
+
+  if (loading)
+    return (
+      <LoadingWrapper>
+        <ActivityIndicator animating text="로딩 중입니다" />
+      </LoadingWrapper>
+    );
+
   return (
     <Wrapper>
       <Title>택시자버 이용내역</Title>
-      {data.userHistory.map(({ id, request, ...info }: any) => (
-        <HistoryBox key={id} {...request} {...info} />
-      ))}
+      {showingHistory.length ? (
+        showingHistory.map(({ id, request, ...info }: any) => <HistoryBox key={id} {...request} {...info} />)
+      ) : (
+        <NoHistory>이용내역이 없습니다</NoHistory>
+      )}
+      <PaginationButtonGrup>
+        <Button disabled={buttonDisabled.prev} onClick={onPrev}>
+          최근내역
+        </Button>
+        <Button disabled={buttonDisabled.next} onClick={onNext}>
+          이전내역
+        </Button>
+      </PaginationButtonGrup>
       <Button
         shape="circle"
         icon={<HomeFilled />}
@@ -41,6 +83,32 @@ const Wrapper = styled.div`
 
 const Title = styled.h1`
   margin: 25px 0 10px 25px;
+`;
+
+const PaginationButtonGrup = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 20px 0 30px;
+
+  > button {
+    margin: 0 20px;
+  }
+`;
+
+const LoadingWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+`;
+
+const NoHistory = styled.h2`
+  text-align: center;
+  padding: 50px 0;
 `;
 
 export default UserHistoryPage;
