f6ef4fd2cd002ffd2112bc70c3d835acf7b234ab
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-12-08 10:43:48 +0900

    [fe] feat (#97) 메뉴 버튼 디자인 수정 - 하위 메뉴 텍스트 삭제

--- client/src/components/common/Menu.tsx
+++ client/src/components/common/Menu.tsx
@@ -5,17 +5,18 @@ import { useMutation } from '@apollo/client';
 import { Menu, Button } from 'antd';
 import { MenuUnfoldOutlined, MenuFoldOutlined, LogoutOutlined, HistoryOutlined } from '@ant-design/icons';
 import { SIGNOUT } from '@queries/signout';
+
 interface MenuPropsType {
   type: string;
 }
 
 const MenuButton: React.FC<MenuPropsType> = ({ type }) => {
   const history = useHistory();
-  const [foldable, setFoldable] = useState(true);
+  const [visible, setVisible] = useState(false);
 
   const toggle = useCallback(() => {
-    setFoldable(!foldable);
-  }, [foldable]);
+    setVisible(!visible);
+  }, [visible]);
 
   const [signout] = useMutation(SIGNOUT);
 
@@ -38,17 +39,17 @@ const MenuButton: React.FC<MenuPropsType> = ({ type }) => {
 
   return (
     <Overlay>
-      <Button type="primary" onClick={toggle} style={{ marginBottom: 16 }}>
-        {React.createElement(foldable ? MenuFoldOutlined : MenuUnfoldOutlined)}
+      <Button type="primary" onClick={toggle} style={{ marginBottom: 16 }} shape="circle">
+        {React.createElement(visible ? MenuFoldOutlined : MenuUnfoldOutlined)}
       </Button>
-      <Menu mode="inline" inlineCollapsed={foldable}>
-        <Menu.Item key="1" icon={<LogoutOutlined />} onClick={logout}>
-          로그아웃
-        </Menu.Item>
-        <Menu.Item key="2" icon={<HistoryOutlined />} onClick={historyHandler}>
-          이용 내역
-        </Menu.Item>
-      </Menu>
+      <>
+        {visible ? (
+          <Menu mode="inline" inlineCollapsed={visible}>
+            <Menu.Item key="1" icon={<LogoutOutlined />} onClick={logout} />
+            <Menu.Item key="2" icon={<HistoryOutlined />} onClick={historyHandler} />
+          </Menu>
+        ) : null}
+      </>
     </Overlay>
   );
 };
@@ -56,7 +57,7 @@ const MenuButton: React.FC<MenuPropsType> = ({ type }) => {
 const Overlay = styled.div`
   position: absolute;
   top: 200px;
-  left: 0px;
+  left: 10px;
   z-index: 999;
 `;
 
