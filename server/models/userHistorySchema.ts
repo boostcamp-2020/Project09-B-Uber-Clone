da2aefafc1fe2ef6ed6a7a63408f19cc915da252
JungSWon <sw.onandon@gmail.com>
2020-12-08 15:56:02 +0900

    [be] feat (#108) userHistorySchema 작성 -  다른 스키마의 네이밍 룰을 따랐습니다. (`user_id`)

---
+++ server/models/userHistorySchema.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+import requestLocationsShema from './requestLocationsShema';
+const userHistorySchema = new mongoose.Schema({
+  user_id: mongoose.Schema.Types.ObjectId,
+  request: requestLocationsShema,
+  fee: { type: Number, required: true, trim: true },
+  startTime: { type: String, required: true, trim: true },
+  endTime: { type: String, required: true, trim: true },
+  carModel: { type: String, required: true, trim: true },
+  plateNumber: { type: String, required: true, trim: true },
+});
+
+export default userHistorySchema;
