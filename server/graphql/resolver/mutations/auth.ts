b74c651b1ea8939a53c0dba57f829e39fbbdc56d
Jinwoo Lee <34625313+GrasshopperBears@users.noreply.github.com>
2020-12-11 16:15:31 +0900

    [be] refactor (#95) auth mutation 에러 핸들링 및 오류 메세지 분리 - try/catch로 에러  처리 - 에러 메세지 분리

--- server/graphql/resolver/mutations/auth.ts
+++ server/graphql/resolver/mutations/auth.ts
@@ -3,33 +3,47 @@ import jwt from 'jsonwebtoken';
 import { logger } from '../../../config/winston';
 import Config from '../../../config';
 
+const DUPLICATION_ERROR = { success: false, message: '중복된 아이디입니다. 다른 아이디로 가입해주세요.' };
+const SIGNUP_ERROR = { success: false, message: '회원가입 중 오류가 발생했습니다' };
+const WRONG_ID = { success: false, message: '존재하지 않는 아이디입니다.' };
+const WRONG_PASSWORD = { success: false, message: '잘못된 비밀번호입니다.' };
+const WRONG_ACCESS = { success: false, message: '유효하지 않은 접근입니다.' };
+
 export default {
   userSignup: async (_, args, { dataSources, res }) => {
-    const hashedPassword = await bcrypt.hash(args.password, Number(Config.BCRYPT_SALT_ROUNDS));
+    try {
+      const hashedPassword = await bcrypt.hash(args.password, Number(Config.BCRYPT_SALT_ROUNDS));
+      const userSchema = dataSources.model('User');
+      const newUser = new userSchema({ ...args, password: hashedPassword });
+      const result = await newUser.save();
 
-    const userSchema = dataSources.model('User');
-    const newUser = new userSchema({ ...args, password: hashedPassword });
-    const result = await newUser.save();
-
-    if (!result) return { success: false, message: '회원가입 중 오류가 발생했습니다' };
-    const token = jwt.sign({ id: result._id }, Config.JWT_SECRET);
-    res.cookie('userToken', token, { signed: true, httpOnly: true });
-    return { success: true };
+      if (!result) return SIGNUP_ERROR;
+      const token = jwt.sign({ id: result._id }, Config.JWT_SECRET);
+      res.cookie('userToken', token, { signed: true, httpOnly: true });
+      return { success: true };
+    } catch (err) {
+      logger.error(`USER SIGNUP ERROR: ${err}`);
+      return SIGNUP_ERROR;
+    }
   },
   driverSignup: async (_, args, { dataSources, res }) => {
-    const hashedPassword = await bcrypt.hash(args.password, Number(Config.BCRYPT_SALT_ROUNDS));
-    const driverSchema = dataSources.model('Driver');
+    try {
+      const hashedPassword = await bcrypt.hash(args.password, Number(Config.BCRYPT_SALT_ROUNDS));
+      const driverSchema = dataSources.model('Driver');
+      const driver = await driverSchema.findOne({ id: args.id });
+      if (driver) return DUPLICATION_ERROR;
 
-    const driver = await driverSchema.findOne({ id: args.id });
-    if (driver) return { success: false, message: '중복된 아이디입니다. 다른 아이디로 가입해주세요.' };
+      const newDriver = new driverSchema({ ...args, password: hashedPassword });
+      const result = await newDriver.save();
+      if (!result) SIGNUP_ERROR;
 
-    const newDriver = new driverSchema({ ...args, password: hashedPassword });
-    const result = await newDriver.save();
-    if (!result) return { success: false, message: '회원가입 중 오류가 발생했습니다' };
-
-    const token = jwt.sign({ id: result._id }, Config.JWT_SECRET);
-    res.cookie('driverToken', token, { signed: true, httpOnly: true });
-    return { success: true };
+      const token = jwt.sign({ id: result._id }, Config.JWT_SECRET);
+      res.cookie('driverToken', token, { signed: true, httpOnly: true });
+      return { success: true };
+    } catch (err) {
+      logger.error(`DRIVER SIGNUP ERROR: ${err}`);
+      return SIGNUP_ERROR;
+    }
   },
   userSignin: async (_, args, { dataSources, res }) => {
     try {
@@ -42,12 +56,12 @@ export default {
           logger.info(`${args.id} user logined!`);
           return { success: true };
         }
-        return { success: false, message: '잘못된 비밀번호입니다.' };
+        return WRONG_PASSWORD;
       }
-      return { success: false, message: '존재하지 않는 아이디입니다.' };
+      return WRONG_ID;
     } catch (err) {
       logger.info('User login error!');
-      return { success: false, message: '유효하지 않은 접근입니다.' };
+      return WRONG_ACCESS;
     }
   },
   driverSignin: async (_, args, { dataSources, res }) => {
@@ -61,12 +75,12 @@ export default {
           logger.info(`${args.id} driver logined!`);
           return { success: true };
         }
-        return { success: false, message: '잘못된 비밀번호입니다.' };
+        return WRONG_PASSWORD;
       }
-      return { success: false, message: '존재하지 않는 아이디입니다.' };
+      return WRONG_PASSWORD;
     } catch (err) {
       logger.info('Driver login error!');
-      return { success: false, message: '유효하지 않은 접근입니다.' };
+      return WRONG_ACCESS;
     }
   },
   signout: async (_, { type }, { dataSources, req, res }) => {
@@ -78,10 +92,10 @@ export default {
       }
       const result = res.clearCookie(`${type}Token`);
       if (result) return { success: true };
-      return { success: false, message: '잘못된 접근입니다.' };
+      return WRONG_ACCESS;
     } catch (err) {
       logger.error('Logout error!');
-      return { success: false, message: '유효하지 않은 접근입니다.' };
+      return WRONG_ACCESS;
     }
   },
 };
