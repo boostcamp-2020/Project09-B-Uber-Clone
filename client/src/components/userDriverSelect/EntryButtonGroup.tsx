4b9c8e36c2070b7b3bee0e0664a37d03ae2a5589
JungSWon <sw.onandon@gmail.com>
2020-12-12 01:12:10 +0900

    [fe] refactor : EntryButtonGroup 불필요 코드 삭제

--- client/src/components/userDriverSelect/EntryButtonGroup.tsx
+++ client/src/components/userDriverSelect/EntryButtonGroup.tsx
@@ -1,32 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Button, WhiteSpace } from 'antd-mobile';
+import { Button } from 'antd-mobile';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 
 const EntryButtonGroup: React.FC<RouteComponentProps> = ({ history }) => {
   const buttonStyle = { boxShadow: 'rgb(134 134 134 / 60%) 3px 6px 20px' };
   return (
     <Div>
-      <Button
-        type="primary"
-        onClick={() => {
-          history.push('/user');
-          return;
-        }}
-        style={buttonStyle}
-      >
+      <Button type="primary" onClick={() => history.push('/user')} style={buttonStyle}>
         사용자로 시작하기
       </Button>
-      <WhiteSpace />
       <br />
-      <Button
-        type="primary"
-        onClick={() => {
-          history.push('/driver');
-          return;
-        }}
-        style={buttonStyle}
-      >
+      <Button type="primary" onClick={() => history.push('/driver')} style={buttonStyle}>
         드라이버로 시작하기
       </Button>
     </Div>
