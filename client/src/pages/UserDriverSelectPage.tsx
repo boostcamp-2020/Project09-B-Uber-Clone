fdb148c1fd1bda589966b0b7d9859c94d83c0f7e
JungSWon <sw.onandon@gmail.com>
2020-12-11 10:48:49 +0900

    [fe] beauty : 유저/드라이버 선택 페이지 수정 - 타이틀과 버튼그룹 위치조정

--- client/src/pages/UserDriverSelectPage.tsx
+++ client/src/pages/UserDriverSelectPage.tsx
@@ -6,7 +6,6 @@ const UserDriverSelectPage: React.FC = () => {
   return (
     <Div>
       <H1>택시자버</H1>
-      {/* <img src="https://camo.githubusercontent.com/bebbca2f4237ace4ed0da7bd0b43b0e2c0b09691d6489f7251fcae7556b55f5b/68747470733a2f2f692e696d6775722e636f6d2f4c4a686d7674332e706e67" /> */}
       <EntryButtonGroup />
     </Div>
   );
@@ -18,10 +17,11 @@ const Div = styled.div`
   align-items: center;
   height: 100vh;
   border: 1px solid;
-  justify-content: space-around;
+  justify-content: space-between;
   background-image: url('https://camo.githubusercontent.com/bebbca2f4237ace4ed0da7bd0b43b0e2c0b09691d6489f7251fcae7556b55f5b/68747470733a2f2f692e696d6775722e636f6d2f4c4a686d7674332e706e67');
   background-repeat: no-repeat;
   background-position: center;
+  padding: 40px;
 `;
 
 const H1 = styled.h1`
