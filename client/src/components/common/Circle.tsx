ba8527d0f66bb5146a36ae71e4bd4cba5e630d8d
JungSWon <sw.onandon@gmail.com>
2020-12-11 10:39:01 +0900

    [fe] fix : Circle Style 옵션명 컨벤션 변경

--- client/src/components/common/Circle.tsx
+++ client/src/components/common/Circle.tsx
@@ -6,28 +6,27 @@ const Circle: React.FC<{ color: string }> = ({ color }) => {
       <g transform="translate(16.856,16.856) scale(0.804,0.804)">
         <g
           fill="none"
-          fill-rule="nonzero"
+          fillRule="nonzero"
           stroke="none"
-          stroke-width="none"
-          stroke-linecap="butt"
-          stroke-linejoin="none"
-          stroke-miterlimit="10"
-          stroke-dasharray=""
-          stroke-dashoffset="0"
-          font-family="none"
-          font-weight="none"
-          font-size="none"
-          text-anchor="none"
+          strokeWidth="none"
+          strokeLinecap="butt"
+          strokeMiterlimit="10"
+          strokeDasharray=""
+          strokeDashoffset="0"
+          fontFamily="none"
+          fontWeight="none"
+          fontSize="none"
+          textAnchor="none"
         >
-          <g fill={color} stroke={color} stroke-width="40" stroke-linejoin="round">
+          <g fill={color} stroke={color} strokeWidth="40" strokeLinejoin="round">
             <path d="M86,157.66667c-39.41667,0 -71.66667,-32.25 -71.66667,-71.66667c0,-39.41667 32.25,-71.66667 71.66667,-71.66667c39.41667,0 71.66667,32.25 71.66667,71.66667c0,39.41667 -32.25,71.66667 -71.66667,71.66667zM86,28.66667c-31.53333,0 -57.33333,25.8 -57.33333,57.33333c0,31.53333 25.8,57.33333 57.33333,57.33333c31.53333,0 57.33333,-25.8 57.33333,-57.33333c0,-31.53333 -25.8,-57.33333 -57.33333,-57.33333z"></path>
           </g>
-          <path d="M0,172v-172h172v172z" fill="none" stroke="none" stroke-width="1" stroke-linejoin="miter"></path>
-          <g fill={color} stroke="none" stroke-width="1" stroke-linejoin="miter">
+          <path d="M0,172v-172h172v172z" fill="none" stroke="none" strokeWidth="1" strokeLinejoin="miter"></path>
+          <g fill={color} stroke="none" strokeWidth="1" strokeLinejoin="miter">
             <path d="M86,157.66667c-39.41667,0 -71.66667,-32.25 -71.66667,-71.66667c0,-39.41667 32.25,-71.66667 71.66667,-71.66667c39.41667,0 71.66667,32.25 71.66667,71.66667c0,39.41667 -32.25,71.66667 -71.66667,71.66667zM86,28.66667c-31.53333,0 -57.33333,25.8 -57.33333,57.33333c0,31.53333 25.8,57.33333 57.33333,57.33333c31.53333,0 57.33333,-25.8 57.33333,-57.33333c0,-31.53333 -25.8,-57.33333 -57.33333,-57.33333z"></path>
           </g>
-          <path d="" fill="none" stroke="none" stroke-width="1" stroke-linejoin="miter"></path>
-          <path d="" fill="none" stroke="none" stroke-width="1" stroke-linejoin="miter"></path>
+          <path d="" fill="none" stroke="none" strokeWidth="1" strokeLinejoin="miter"></path>
+          <path d="" fill="none" stroke="none" strokeWidth="1" strokeLinejoin="miter"></path>
         </g>
       </g>
     </svg>
