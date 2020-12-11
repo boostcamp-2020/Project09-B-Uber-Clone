c52bad4284f50bb18cd92c1565fa6f7daec1329e
JungSWon <sw.onandon@gmail.com>
2020-12-12 01:26:00 +0900

    [fe] refactor : AuthButtonGroup 불필요 코드 삭제

--- client/src/components/userDriverSelect/EntryButtonGroup.tsx
+++ client/src/components/userDriverSelect/EntryButtonGroup.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Button } from 'antd-mobile';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 
 const EntryButtonGroup: React.FC<RouteComponentProps> = ({ history }) => {
   const buttonStyle = { boxShadow: 'rgb(134 134 134 / 60%) 3px 6px 20px' };
   return (
-    <Div>
+    <div style={{ width: '80%' }}>
       <Button type="primary" onClick={() => history.push('/user')} style={buttonStyle}>
         사용자로 시작하기
       </Button>
@@ -14,12 +13,8 @@ const EntryButtonGroup: React.FC<RouteComponentProps> = ({ history }) => {
       <Button type="primary" onClick={() => history.push('/driver')} style={buttonStyle}>
         드라이버로 시작하기
       </Button>
-    </Div>
+    </div>
   );
 };
-
-const Div = styled.div`
-  width: 80%;
-`;
 
 export default withRouter(EntryButtonGroup);
