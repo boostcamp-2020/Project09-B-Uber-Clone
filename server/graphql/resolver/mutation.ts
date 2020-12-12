001cadbf5190fc07af78b1c50e13b5086cbc1392
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-12-12 23:59:44 +0900

    [be] refactor (#95) 로그인 뮤테이션 리팩토링 - 드라이버/유저 로그인 뮤테이션을 하나로 통합 - type(user/driver)에 따라 해당 스키마에서 아이디와 비밀번호를 조회

--- server/graphql/resolver/mutation.ts
+++ server/graphql/resolver/mutation.ts
@@ -33,34 +33,15 @@ const Mutation = {
     res.cookie('driverToken', token, { signed: true, httpOnly: true });
     return { success: true };
   },
-  userSignin: async (_, args, { dataSources, res }) => {
+  signin: async (_, { type, id, password }, { dataSources, res }) => {
     try {
-      const userSchema = dataSources.model('User');
-      const user = await userSchema.findOne({ id: args.id });
-      if (user) {
-        if (await bcrypt.compare(args.password, user.password)) {
-          const token = jwt.sign({ id: user._id }, Config.JWT_SECRET);
-          res.cookie('userToken', token, { httpOnly: true, signed: true });
-          logger.info(`${args.id} user logined!`);
-          return { success: true };
-        }
-        return { success: false, message: '잘못된 비밀번호입니다.' };
-      }
-      return { success: false, message: '존재하지 않는 아이디입니다.' };
-    } catch (err) {
-      logger.info('User login error!');
-      return { success: false, message: '유효하지 않은 접근입니다.' };
-    }
-  },
-  driverSignin: async (_, args, { dataSources, res }) => {
-    try {
-      const driverSchema = dataSources.model('Driver');
-      const driver = await driverSchema.findOne({ id: args.id });
-      if (driver) {
-        if (await bcrypt.compare(args.password, driver.password)) {
-          const token = jwt.sign({ id: driver._id }, Config.JWT_SECRET);
-          res.cookie('driverToken', token, { httpOnly: true, signed: true });
-          logger.info(`${args.id} driver logined!`);
+      const schema = type === 'user' ? dataSources.model('User') : dataSources.model('Driver');
+      const result = await schema.findOne({ id });
+      if (result) {
+        if (await bcrypt.compare(password, result.password)) {
+          const token = jwt.sign({ id: result._id }, Config.JWT_SECRET);
+          res.cookie(`${type}Token`, token, { httpOnly: true, signed: true });
+          logger.info(`${id} ${type} logined!`);
           return { success: true };
         }
         return { success: false, message: '잘못된 비밀번호입니다.' };
