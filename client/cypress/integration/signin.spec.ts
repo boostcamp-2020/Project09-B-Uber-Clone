218478267ed34b22155a7985e318b9897a822152
2hoyeong <leehodud302@naver.com>
2020-12-14 01:20:29 +0900

    [fe] feat (#149) 드라이버 로그인 테스트 작성
    - 드라이버 auth 페이지 버튼 동작 테스트
    - 드라이버 아이디 비밀번호 잘 못 입력 실패 테스트
    - 드라이버 아이디 비밀번호 제대로 입력하면 성공 테스트

--- client/cypress/integration/signin.spec.ts
+++ client/cypress/integration/signin.spec.ts
@@ -44,3 +44,50 @@ describe('승객 로그인 페이지 테스트', () => {
     cy.url().should('eq', `${Cypress.config().baseUrl}/user/map`);
   });
 });
+
+describe('드라이버 로그인 페이지 테스트', () => {
+  before(() => {
+    cy.visit('/driver');
+    cy.clearCookies();
+    cy.wait(3000);
+  });
+
+  it('회원가입 버튼을 클릭한다', () => {
+    cy.get("[data-test-id='sign-up-btn']").click();
+    cy.url().should('eq', `${Cypress.config().baseUrl}/driver/signup`);
+  });
+
+  it('로그인 버튼을 클릭한다', () => {
+    cy.go('back');
+    cy.get("[data-test-id='sign-in-btn']").click();
+    cy.url().should('eq', `${Cypress.config().baseUrl}/driver/signin`);
+  });
+
+  it('잘못된 아이디 비밀번호를 입력하면 로그인에 실패한다.', () => {
+    cy.get("[data-test-id='id-input']").clear().type(Cypress.env('driver_id'));
+    cy.get("[data-test-id='pw-input']")
+      .clear()
+      .type(Cypress.env('driver_pw') + 's');
+
+    const stub = cy.stub();
+
+    cy.on('window:alert', stub);
+
+    cy.get('a')
+      .contains('로그인')
+      .click()
+      .then(() => {
+        cy.wait(1000);
+      })
+      .then(() => {
+        expect(stub.getCall(0)).to.be.calledWith('잘못된 비밀번호입니다.');
+      });
+  });
+
+  it('올바른 아이디 비밀번호를 입력하면 로그인에 성공한다.', () => {
+    cy.get("[data-test-id='id-input']").clear().type(Cypress.env('driver_id'));
+    cy.get("[data-test-id='pw-input']").clear().type(Cypress.env('driver_pw'));
+    cy.get('a').contains('로그인').click();
+    cy.url().should('eq', `${Cypress.config().baseUrl}/driver/main`);
+  });
+});
